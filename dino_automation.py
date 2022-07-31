0a4a6d4b04bdd2352c0759b6eb973fdb1f5a2838
niteshRdangal <niteshrd1@gmail.com>
2022-07-31 14:43:42 +0545

    The dino is jumping

--- dino_automation.py
+++ dino_automation.py
@@ -6,21 +6,31 @@ from numpy import asarray
 def hit(key):
     pyautogui.keyDown(key)
 
-
 def draw():
     pass
 
-def screenShot():
-    image =  ImageGrab.grab().convert('L')
-    return image
+def isCollide(data):
+    for i in range(300,420):
+            for j in range(605,657):
+                if data[i, j] >150:
+                    return True
+    return False
+
 
 
 if __name__ == "__main__":
-    time.sleep(1)
-    image = screenShot()
-    data = image.load() 
-    print(asarray(image))
-    for i in range(34,45):
-        for j in range(45,67):
-            data
+    print("The game is about to start")
+    time.sleep(3)
+    hit("up")
+    while True:
+        image =  ImageGrab.grab().convert('L') # converted the imgae into grayscale
+        data = image.load() 
+        if isCollide(data):
+            hit("up")
+        #print(asarray(image)) # numpy asarray stored the image in array form
+        # for i in range(300,420):
+        #     for j in range(605,657):
+        #         data[i, j] = 255
+
+        #image.show()
     
